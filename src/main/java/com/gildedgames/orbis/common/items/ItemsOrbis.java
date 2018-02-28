7b7316075543c2dbb4481e71efe521786c758212
Brandon Pearce <kingbdogz_war3freak@hotmail.com>
2018-02-28 20:00:49 +0800

    fix: Removed debug tab.

--- src/main/java/com/gildedgames/orbis/common/items/ItemsOrbis.java
+++ src/main/java/com/gildedgames/orbis/common/items/ItemsOrbis.java
@@ -2,11 +2,8 @@ package com.gildedgames.orbis.common.items;
 
 import com.gildedgames.orbis.common.OrbisCore;
 import net.minecraft.block.Block;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -24,15 +21,6 @@ public class ItemsOrbis
 	public static final ItemBlueprintPalette blueprint_palette = new ItemBlueprintPalette();
 
 	public static final ItemEntity entity_item = new ItemEntity();
-
-	public static final CreativeTabs ENTITY_TAB = new CreativeTabs("Entities")
-	{
-		@Override
-		public ItemStack getTabIconItem()
-		{
-			return new ItemStack(Items.DIAMOND_HORSE_ARMOR);
-		}
-	};
 
 	@SubscribeEvent
 	public static void onRegisterItems(final RegistryEvent.Register<Item> event)
