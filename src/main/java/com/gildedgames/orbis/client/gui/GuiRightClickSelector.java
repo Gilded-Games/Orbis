9ea0f4cfaa36b6d5f5957e22b45e37247ad30a6a
Brandon Pearce <kingbdogz_war3freak@hotmail.com>
2018-03-02 03:34:21 +0800

    feature: "Remove" entry added to dropdown menu for Select Regions.

--- src/main/java/com/gildedgames/orbis/client/gui/GuiRightClickSelector.java
+++ src/main/java/com/gildedgames/orbis/client/gui/GuiRightClickSelector.java
@@ -1,11 +1,19 @@
 package com.gildedgames.orbis.client.gui;
 
+import com.gildedgames.orbis.api.world.WorldObjectManager;
+import com.gildedgames.orbis.client.gui.data.DropdownElement;
 import com.gildedgames.orbis.client.gui.util.GuiDropdownList;
 import com.gildedgames.orbis.client.gui.util.GuiFrame;
 import com.gildedgames.orbis.client.rect.Dim2D;
 import com.gildedgames.orbis.client.rect.Pos2D;
+import com.gildedgames.orbis.common.capabilities.player.PlayerOrbis;
+import com.gildedgames.orbis.common.network.NetworkingOrbis;
+import com.gildedgames.orbis.common.network.packets.PacketClearSelectedRegion;
+import com.gildedgames.orbis.common.network.packets.PacketWorldObjectRemove;
 import com.gildedgames.orbis.common.world_objects.WorldShape;
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.text.TextComponentString;
 
 import java.io.IOException;
 
@@ -13,10 +21,13 @@ public class GuiRightClickSelector extends GuiFrame
 {
 	private final WorldShape region;
 
-	public GuiRightClickSelector(final WorldShape region)
+	private PlayerOrbis playerOrbis;
+
+	public GuiRightClickSelector(PlayerOrbis playerOrbis, final WorldShape region)
 	{
 		super(null, Dim2D.flush());
 
+		this.playerOrbis = playerOrbis;
 		this.region = region;
 	}
 
@@ -26,6 +37,20 @@ public class GuiRightClickSelector extends GuiFrame
 		this.addChildren(new GuiDropdownList(Pos2D.flush(this.width / 2, this.height / 2),
 				GuiRightClickElements.delete(this.region),
 				GuiRightClickElements.copy(this.region),
+				new DropdownElement(new TextComponentString("Remove"))
+				{
+					@Override
+					public void onClick(final GuiDropdownList list, final EntityPlayer player)
+					{
+						final WorldObjectManager manager = WorldObjectManager.get(player.world);
+
+						NetworkingOrbis.sendPacketToServer(new PacketClearSelectedRegion());
+						NetworkingOrbis.sendPacketToServer(new PacketWorldObjectRemove(GuiRightClickSelector.this.region.getWorld(), manager.getGroup(0),
+								GuiRightClickSelector.this.region));
+
+						GuiRightClickSelector.this.playerOrbis.powers().getSelectPower().setSelectedRegion(null);
+					}
+				},
 				GuiRightClickElements.close()));
 	}
 
